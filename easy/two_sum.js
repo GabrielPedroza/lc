ed47161e979962a6cb4d04311156c79beb6ff897
Gabriel Pedroza <theegabrielpedroza@gmail.com>
2023-11-24 15:11:17 -0500

    init

--- easy/two_sum.js
+++ easy/two_sum.js
@@ -1,2 +1,13 @@
-const twoSum = () => {
+const twoSum = (nums, target) => {
+	const storage = new Map()
+
+	for (const [index, num] of Object.entries(nums) {
+		const potentialNum = target - num
+		if (storage.has(potentialNum)) {
+			return [index, storage.get(potentialNum)]
+		} else {
+			storage.set(num, index)
+		}
+	}
+	return []
 }
